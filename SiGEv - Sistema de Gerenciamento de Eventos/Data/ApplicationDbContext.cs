cc8e4432583a89f1798a8bdceb0651fe9364d476
Gustavo Girão <gustavogiraobg@gmail.com>
2022-05-25 22:22:32 -0300

    Started Project

---
+++ SiGEv - Sistema de Gerenciamento de Eventos/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SiGEv___Sistema_de_Gerenciamento_de_Eventos.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ SiGEv - Sistema de Gerenciamento de Eventos/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SiGEv___Sistema_de_Gerenciamento_de_Eventos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
