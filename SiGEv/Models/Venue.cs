eb28e1e7b07a57fa6a4bb735dc220de7c5a42ce5
Gustavo Girão <gustavogiraobg@gmail.com>
2022-06-25 21:47:44 -0300

    feat: add event list in view

--- SiGEv/Models/Venue.cs
+++ SiGEv/Models/Venue.cs
@@ -1,10 +1,13 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace SiGEv.Models
 {
     public class Venue
     {
         public int Id { get; set; }
         public int TotalSeats { get; set; }
+
+        [Display(Name = "Endereço")]
         public string Adress { get; set; }
         public string Description { get; set; }
 
