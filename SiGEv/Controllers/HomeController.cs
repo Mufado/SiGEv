ac363d877104e263f0d40db99f9f8db0f5468f12
Jos3s <karatedkgt17@gmail.com>
2022-07-02 20:55:21 -0300

    chore: fix name

--- SiGEv/Controllers/HomeController.cs
+++ SiGEv/Controllers/HomeController.cs
@@ -19,7 +19,7 @@ namespace SiGEv.Controllers
             return View();
         }
 
-        public IActionResult Privacidade()
+        public IActionResult Privacy()
         {
             return View();
         }
