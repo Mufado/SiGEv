22d58a1097569b6f41d31472ae239f61dbab6a5d
Jos3s <karatedkgt17@gmail.com>
2022-07-09 23:44:05 -0300

    feat: create inicial cart view

---
+++ SiGEv/Controllers/CartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SiGEv.Controllers
+{
+	public class CartController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ SiGEv/Models/ViewModels/CartFormViewModel.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace SiGEv.Models.ViewModels
+{
+	public class CartFormViewModel
+	{
+		public ICollection<Event> Events { get; set; }
+	}
+}
