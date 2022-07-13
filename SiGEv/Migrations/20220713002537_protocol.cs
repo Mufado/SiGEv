a365ae999e59e741d8950a160651f8676dd07391
Jos3s <karatedkgt17@gmail.com>
2022-07-12 21:31:18 -0300

    feat: new migration

---
+++ SiGEv/Migrations/20220713002537_protocol.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SiGEv.Migrations
+{
+    public partial class protocol : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Protocol",
+                table: "Bills",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DocumentType",
+                table: "Bills",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DocumentType",
+                table: "Bills");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Protocol",
+                table: "Bills",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
