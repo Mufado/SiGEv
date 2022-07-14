768b48e6b782cae86c2f2dc4055784d4a1b80e46
André Vinicius Santos de Lima <aviniciuslima@gmail.com>
2022-07-13 21:44:18 -0300

    DEU CERTO PORRA

--- SiGEv/Controllers/ReportsController.cs
+++ SiGEv/Controllers/ReportsController.cs
@@ -7,9 +7,11 @@ using System.Dynamic;
 using System.Threading.Tasks;
 using SiGEv.Services;
 using SiGEv.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 
 namespace SiGEv.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class ReportsController : Controller
     {
         private readonly ReportsService _reportsService;
@@ -22,13 +24,11 @@ namespace SiGEv.Controllers
             _eventsService = eventsService;
         }
 
-        public async Task<IActionResult> Index(DateTime? date)
+        public IActionResult Index(DateTime? date)
         {
-			var events = _eventsService.GetAllEvents();
-            var bills = await _reportsService.GetProfitByDateAsync(date);
-            var viewModel = new ReportsFormViewModel { ListBills = bills, ListEvents = events };
+			var list = _reportsService.GetProfitByDateAsync(date);
 
-            return View(viewModel);
+            return View(list);
         }
     }
 }
--- SiGEv/Services/ReportsService.cs
+++ SiGEv/Services/ReportsService.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using SiGEv.Models.ViewModels;
 
 namespace SiGEv.Services
 {
@@ -17,19 +18,23 @@ namespace SiGEv.Services
             _context = context;
         }
 
-        public async Task<List<Bill>> GetProfitByDateAsync(DateTime? dateSearch)
+        public IQueryable<ReportsFormViewModel> GetProfitByDateAsync(DateTime? dateSearch)
         {
-            var result = from obj in _context.Bills select obj;
+            var billsList = _context.Bills;
+            var eventsList = _context.Events;
+            var ticketsList = _context.Tickets;
+            var sectionsList = _context.Sections;
 
-            if (dateSearch.HasValue)
-            {
-                result = result.Where(x => x.PaymentDate >= dateSearch.Value);
-            }
+            var result = from b in billsList
+                         join t in ticketsList on b.Id equals t.BillId into table1
+                         from t in table1.DefaultIfEmpty()
+                         join s in sectionsList on t.SectionId equals s.Id into table2
+                         from s in table2.DefaultIfEmpty()
+                         join e in eventsList on s.EventId equals e.Id into table3
+                         from e in table3.DefaultIfEmpty()
+                         select new ReportsFormViewModel { Bill = b, Event = e, Ticket = t, Section = s };
 
-            return await result
-                .Include(x => x.SelledTickets)
-                .OrderByDescending(x => x.PaymentDate)
-                .ToListAsync();
+            return result;
         }
     }
 }
